4d2ce1cb18963f85ba5adfac8bdcfa5ba60f1d45
vitkarpov <viktor.s.karpov@gmail.com>
2018-07-15 11:54:11 +0300

    refactor: one pass
    Could've been done in one pass. Also it's not necessary to check the indexes anymore since the value adds to hackmap after the check.

--- Numbers add up to a given total/index.js
+++ Numbers add up to a given total/index.js
@@ -5,15 +5,18 @@ assert(isThereAPairOfNumbers([10, -5, 3, 7], 5));
 assert(isThereAPairOfNumbers([10, -5, 3, 5], 0));
 assert(isThereAPairOfNumbers([-10, -5, 3, 7], -15));
 
+assert(!isThereAPairOfNumbers([5, 10, 3, 4], 5));
 assert(!isThereAPairOfNumbers([10, 5, 3, 7], 0));
 assert(!isThereAPairOfNumbers([10, 15, 3, 7], 14));
 
 function isThereAPairOfNumbers(arr, k) {
-    const compl = arr.reduce((acc, item, index) => {
-        return acc[k - item] = index, acc;
-    }, {});
+    const compl = {};
 
-    return arr.filter((item, index) => {
-        return typeof compl[item] !== 'undefined' && compl[item] !== index;
-    }).length > 0;
+    return arr.reduce((acc, item, index) => {
+        if (compl[item]) {
+            return true;
+        }
+        compl[k - item] = true;
+        return acc;
+    }, false);
 }
