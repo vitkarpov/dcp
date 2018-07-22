ddd51ae60c7528557f2d0c44833d6b5e7c1df807
vitkarpov <viktor.s.karpov@gmail.com>
2018-07-22 10:50:40 +0300

    refactor: not necessary check
    There's no such cases according the problem statement.

--- Number of ways to decode the message/index.js
+++ Number of ways to decode the message/index.js
@@ -8,9 +8,6 @@ function count(s, k = 0, dp = []) {
     if (k === s.length) {
         return 1;
     }
-    if (s[k] === '0') {
-        return 0;
-    }
     if (dp[k]) {
         return dp[k];
     }
