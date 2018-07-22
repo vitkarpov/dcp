d0993a83672bbefe95509721f345c4b3ff8baeda
vitkarpov <viktor.s.karpov@gmail.com>
2018-07-22 10:49:37 +0300

    fix: incorrect offset
    count('111') should be equal 3, not 2

--- Number of ways to decode the message/index.js
+++ Number of ways to decode the message/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const assert = require('assert');
+
+assert(count('111') === 3);
+
 function count(s, k = 0, dp = []) {
     if (k === s.length) {
         return 1;
@@ -13,7 +17,7 @@ function count(s, k = 0, dp = []) {
 
     let result = count(s, k + 1, dp);
 
-    if (s.length - k >= 2 && Number(s[k + 1] + s[k + 2]) <= 26) {
+    if (s.length - k >= 2 && Number(s[k] + s[k + 1]) <= 26) {
         result += count(s, k + 2, dp);
     }
     dp[k] = result;
