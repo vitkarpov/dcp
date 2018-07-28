89badc2747b6504ff54dc4e91bc76f5adbc42178
vitkarpov <viktor.s.karpov@gmail.com>
2018-07-28 13:10:41 +0300

    chore: fix a typo

--- PI Approximation/index.js
+++ PI Approximation/index.js
@@ -1,6 +1,6 @@
 /**
  * The key idea here is that you can uniformly distribute points
- * on the NxN square area and then count how many points ended up
+ * on the 1x1 square area and then count how many points ended up
  * inside and outside the inscribed circle (but inside the square).
  * As soos as points are well distributed the approximation should be accurate.
  */
