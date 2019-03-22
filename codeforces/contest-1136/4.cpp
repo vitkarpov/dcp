36732cb28f131897a394be2e5b33971d1b3f1f44
vitkarpov <viktor.karpov@toptal.com>
2019-03-22 17:42:18 +0300

    Editorial solution
    https://codeforces.com/blog/entry/65905

--- codeforces/contest-1136/4.cpp
+++ codeforces/contest-1136/4.cpp
@@ -1,64 +1,51 @@
-#include <stdio.h>
 #include <algorithm>
+#include <iostream>
 #include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 int main() {
+  freopen("foo", "r", stdin);
   int n, m;
-  scanf("%d %d", &n, &m);
+  cin >> n >> m;
   vector<int> people(n);
-  unordered_map<int, int> people_index(n);
-  vector<unordered_map<int, bool>> pairs(n + 1);
-  vector<int> p;
+  vector<int> was(n);
+  vector<vector<int>> pairs(n);
 
   for (int i = 0; i < n; i++) {
-    int p;
-    scanf("%d", &p);
-    people[i] = p;
-    people_index[p] = i;
+    cin >> people[i];
+    people[i]--;
   }
-
-  int target = people[n - 1];
 
   for (int i = 0; i < m; i++) {
     int u, v;
-    scanf("%d %d", &u, &v);
-    pairs[u][v] = true;
-    if (v == target) {
-      p.push_back(u);
+    cin >> u >> v;
+    u--;
+    v--;
+    pairs[u].push_back(v);
+  }
+
+  reverse(people.begin(), people.end());
+  int ans = 0;
+
+  for (auto i : people) cout << i << ' ';
+  cout << endl;
+
+  was[people[0]] = 1;
+  int cnt = 1;
+  for (int i = 1; i < n; i++) {
+    int cnt2 = 0;
+    for (int to : pairs[people[i]]) {
+      if (was[to]) cnt2++;
+    }
+    if (cnt == cnt2) {
+      ans++;
+    } else {
+      was[people[i]] = 1;
+      cnt++;
     }
   }
 
-  vector<pair<int, int>> t;
-  for (int i = 0; i < p.size(); i++) {
-    t.push_back({people_index[p[i]], p[i]});
-  }
-  sort(t.begin(), t.end());
-
-  int swaps = 0;
-  unordered_map<int, bool> erasedPos;
-
-  for (auto it = t.rbegin(); it != t.rend(); ++it) {
-    int currPos = it->first;
-    int curr = it->second;
-    bool canSwap = true;
-
-    for (int i = currPos + 1; i < n - 1; i++) {
-      if (erasedPos[i]) {
-        continue;
-      }
-      if (!pairs[curr][people[i]]) {
-        canSwap = false;
-        break;
-      }
-    }
-    if (canSwap) {
-      erasedPos[currPos] = true;
-      swaps++;
-    }
-  }
-
-  printf("%d", swaps);
+  cout << ans << endl;
 }
