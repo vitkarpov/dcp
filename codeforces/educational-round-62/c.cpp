73be86d4ca201af3954cf65a3a655b59e860908c
vitkarpov <viktor.karpov@toptal.com>
2019-03-23 09:24:58 +0300

    fix: test 3 fails because of overflow

--- codeforces/educational-round-62/c.cpp
+++ codeforces/educational-round-62/c.cpp
@@ -1,44 +1,51 @@
+#include <algorithm>
 #include <iostream>
+#include <queue>
 #include <utility>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
+
+#define ull unsigned long long int
+
+struct Song {
+  int time;
+  int beauty;
+  Song(int _t, int _b) : time(_t), beauty(_b){};
+  bool operator<(const Song& rhs) const { return beauty > rhs.beauty; }
+};
 
 int main() {
   int n, k;
   cin >> n >> k;
 
-  vector<pair<long, pair<long, long>>> songs;
+  vector<Song> songs;
   for (int i = 0; i < n; i++) {
-    long t, b;
+    int t, b;
     cin >> t >> b;
-    songs.push_back({-b, {t, 0}});
+    songs.push_back({t, b});
   }
 
   sort(songs.begin(), songs.end());
 
-  long sum = 0;
-  for (int i = 0; i < n; i++) {
-    long b = songs[i].first;
-    long t = songs[i].second.first;
+  vector<ull> sums(n);
+  sums[0] = (ull)songs[0].time;
 
-    sum += t;
-    songs[i] = {-b, {t, sum}};
+  for (int i = 1; i < n; i++) {
+    sums[i] = sums[i - 1] + (ull)songs[i].time;
   }
-  long ans = 0;
-  long sum_short = 0;
-  vector<long> heap;
+
+  ull ans = 0;
+  ull prev_smallest_sum = 0;
+  priority_queue<int, vector<int>, greater<int>> time_heap;
+
   for (int i = 0; i < n; i++) {
-    heap.push_back(-songs[i].second.first);
-    push_heap(heap.begin(), heap.end());
+    time_heap.push(songs[i].time);
     if (i >= k) {
-      pop_heap(heap.begin(), heap.end());
-      sum_short -= heap.back();
-      heap.pop_back();
+      prev_smallest_sum += (ull)time_heap.top();
+      time_heap.pop();
     }
-    long curr = (songs[i].second.second - sum_short) * songs[i].first;
-    ans = max(ans, curr);
+    ans = max(ans, (sums[i] - prev_smallest_sum) * songs[i].beauty);
   }
   cout << ans << endl;
 }
